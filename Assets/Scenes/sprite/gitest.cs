8a5c0684fa28451566c2951be223b7ade084a8f0
tonzinonin <58870791+tonzinonin@users.noreply.github.com>
2023-05-17 13:10:39 +0800

    第二次提交

--- Assets/Scenes/sprite/gitest.cs
+++ Assets/Scenes/sprite/gitest.cs
@@ -7,7 +7,7 @@ public class gitest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("github");
+        Debug.Log("github2");
     }
 
     // Update is called once per frame
